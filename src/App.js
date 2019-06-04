1262189dd3ba7d7219f8f5be9471744df295c8de
shunsuke_sakurai <shunsuke_sakurai@gmail.com>
2019-06-04 20:25:36 +0900

    Make qcode browse test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import React from 'react';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import QRCode from 'qrcode';
+
 
 function App() {
+  const [url, setUrl] = useState("");
+  useEffect(() => {
+    QRCode.toDataURL("https://www.npmjs.com/package/react-app-polyfill")
+    .then(url => {
+      console.log(url)
+      setUrl(url);
+    })
+    .catch(err => {
+      console.error(err)
+    })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={url} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href={url}
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
+
       </header>
     </div>
   );
